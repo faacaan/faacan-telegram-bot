72694d3dff869a8b03a78a83ef4f1c0232b46103
faacaan <fiath.peti@gmail.com>
2025-06-27 22:38:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ try:
     print("✅ Üzenet sikeresen elküldve.")
 except Exception as e:
     print(f"❌ Hiba az üzenetküldés során: {e}")
+import telegram
+import os
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+bot = telegram.Bot(token=TOKEN)
+
+# Kikérjük az utolsó üzeneteket
+updates = bot.get_updates()
+
+# Listázzuk a chat ID-ket
+for update in updates:
+    print("📩 Chat ID:", update.message.chat.id)
+
